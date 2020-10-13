6b416393b0331e732ec68bbb938fea00c834f271
Terry Sanchez <terry.sanchez@gmail.con>
2020-10-13 12:58:33 -0500

    at media

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,21 +14,23 @@ export default function Home({data}) {
       <meta name="description" content="Building a Capstone website" />
     </Helmet>   
 
-    <h1><i className="fa fa-star-o" aria-hidden="true"></i> Vintage Political Buttons <i className="fa fa-star-o" aria-hidden="true"></i></h1>    
-    <img class="img-fluid hero-banner" src="buttons.jpg" alt="buttons"/>  
+    <h1><i className="fa fa-star-o" aria-hidden="true"></i> Vintage Political Buttons <i className="fa fa-star-o" aria-hidden="true"></i></h1> 
+    <div>
+    <img className="img-fluid" src="buttons.jpg" alt="buttons"/> 
+    </div>    
     <br/>    
 
-    <div class="container">
-      <div class="row">
+    <div className="container">
+      <div className="row">
       {data.allContentfulProduct.nodes.map((node, index) => (
-        <div class="col-md-4">
-          <div class="card mb-4 shadow-sm">
-          <img class="card-img-top" src={ node.image.file.url } alt= { node.name }/> 
-            <div class="card-body">
-              <p class="card-text">{node.name}!</p>
-              <div class="d-flex justify-content-between align-items-center card-body">
-                <div class="btn-group">
-                <a href={node.name} class="myButton">View</a>                
+        <div className="col-md-4">
+          <div className="card mb-4 shadow-sm">
+          <img className="card-img-top" src={ node.image.file.url } alt= { node.name }/> 
+            <div className="card-body">
+              <p className="card-text">{node.name}!</p>
+              <div className="d-flex justify-content-between align-items-center card-body">
+                <div className="btn-group">
+                <a href={node.name} className="myButton">View</a>                
                 </div>
                 <i className="fa fa-star" aria-hidden="true"></i>
                 <br/>
